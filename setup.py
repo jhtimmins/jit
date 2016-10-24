679781f16a8ddba3ea035e5c9321e92f110d8257
jhtimmins <james@tradesy.com>
2016-10-23 23:15:03 -0700

    add attribution to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='jit',
 	version='0.1',
+	author='James Timmins',
+	license='MIT',
 	packages=find_packages(),
 	py_modules=['jit'],
 	include_package_data=True,
